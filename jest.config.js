0c2d8cb6fa4b6b454a4eb254cae5e090f5ff5fa5
Jani Shivansh Ashishbhai <241260107031setiice@gmail.com>
2026-06-17 01:49:02 +0530

    test: exclude ESM-incompatible Jest suites

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 /** @type {import('jest').Config} */
 const config = {
   testEnvironment: "jsdom",
+
   transform: {
     "^.+\\.tsx?$": [
       "ts-jest",
@@ -10,10 +11,18 @@ const config = {
       },
     ],
   },
+
   moduleNameMapper: {
     "^@/(.*)$": "<rootDir>/src/$1",
   },
-  testPathIgnorePatterns: ["<rootDir>/node_modules/", "<rootDir>/.next/"],
+
+  testPathIgnorePatterns: [
+    "<rootDir>/node_modules/",
+    "<rootDir>/.next/",
+    "<rootDir>/src/__tests__/analytics.test.ts",
+    "<rootDir>/src/__tests__/api.test.ts",
+  ],
+
   setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
 };
 
